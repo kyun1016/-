a2c877e7708ceec5f56a32fde595ad380fafa929
park seung kyun <psg9710@naver.com>
2019-04-30 21:03:46 +0900

    12796: Add base code

---
+++ 12796 나의 행렬곱셈 답사기.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int arr[101];
+
+void matrix(int K) {
+	cout << 3 << endl;
+	cout << "1 1 1 " << K + 1;
+}
+
+int main() {
+	int K;
+	cin >> K;
+
+	matrix(K);
+
+	return 0;
+}
